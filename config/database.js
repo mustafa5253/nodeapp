843657f589d0ad118e128fa35113186e54f0276b
Arpit Meena <apnodedev@gmail.com>
2018-01-07 21:23:52 +0530

    +Arpit | Gitlab code pipeline error resolved

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@
 module.exports = {
 
     development : 'mongodb://arpit:12345@ds117935.mlab.com:17935/learningapp',
+    production : 'mongodb://apmeena:12345@ds245357.mlab.com:45357/live-db',
 
 };
