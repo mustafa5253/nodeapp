03e034f5459da19db8db88c0d1a138012231e88b
Arpit Meena <apnodedev@gmail.com>
2018-02-12 22:32:33 +0530

    Arpit | File download functionality added

--- routes/file.route.js
+++ routes/file.route.js
@@ -5,7 +5,9 @@ var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var fs = require('fs');
 var path = require('path');
-
+var mmm = require('mmmagic');
+let Magic = mmm.Magic;
+let magic = new Magic(mmm.MAGIC_MIME_TYPE);
 
 /*
  * GET ALL
@@ -26,6 +28,30 @@ router.get('/:id', function (req, res) {
     } else {
         fs.createReadStream(path.join('api-assets', 'e6b65b74e544696704044b42e859953e')).pipe(res);
     }
+});
+
+/*
+ * DOWNLOAD
+ */
+router.get('/download/:id', function (req, res) {
+    let response = {};
+    fs.readFile(path.join('uploads', req.params.id), function(err, data) {
+        if(err) {
+            response.status = 'error';
+            response.data = 'Some error while downloading file.';
+            res.send(response);
+        } else {
+            let base64data = new Buffer(data).toString('base64');
+            magic.detectFile(path.join('uploads', req.params.id), function(error, result) {
+                if(!error) {
+                    response.file_type = result;
+                }
+                response.status = 'success';
+                response.data = base64data;
+                res.send(response);
+            });
+        }
+    });
 });
 
 /*
